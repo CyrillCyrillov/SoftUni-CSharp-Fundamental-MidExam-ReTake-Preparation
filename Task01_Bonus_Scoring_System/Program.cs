c5eef1e4d3c0bcdaca0a3b15a5e00c6db410f32b
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-24 23:08:12 +0200

    Add files via upload

---
+++ Task01_Bonus_Scoring_System/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Task01_Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double studentsNumbers = double.Parse(Console.ReadLine());
+            double lecturesCount = double.Parse(Console.ReadLine());
+            double additionalBonus = double.Parse(Console.ReadLine());
+
+            double maxBonusPoint = 0;
+            int attendancesLectures = 0;
+
+            for (int i = 1; i <= studentsNumbers; i++)
+            {
+                int nextStudentInfo = int.Parse(Console.ReadLine());
+                double nextBonusPoints = Math.Round(nextStudentInfo / lecturesCount * (5 + additionalBonus));
+
+                if(nextBonusPoints > maxBonusPoint)
+                {
+                    maxBonusPoint = nextBonusPoints; ;
+                    attendancesLectures = nextStudentInfo;
+                }
+            }
+            
+            Console.WriteLine($"Max Bonus: {maxBonusPoint}.");
+            Console.WriteLine($"The student has attended {attendancesLectures} lectures.");
+        }
+    }
+}
